6d4bab4d9368d2687eefff25b614f724c4efbfff
huangmy233 <huangmy233@gmail.com>
2024-11-15 23:07:41 -0800

    23.07

--- include/Compute.h
+++ include/Compute.h
@@ -20,13 +20,30 @@ void ProcessingElement(Stream<ComputePackN_t> &aIn,
 
 // Tucker configuration structure
 struct TuckerConfig {
-    unsigned batch_size;
-    unsigned in_features;
-    unsigned out_features;
-    unsigned rank1;
-    unsigned rank2;
-    unsigned rank3;
-    unsigned pe_array_size;
+    // 基本维度参数
+    int batch_size;       // 批大小
+    int in_features;      // 输入特征维度
+    int out_features;     // 输出特征维度
+    
+    // Tucker分解的秩参数
+    int rank1;           // 第一个模式的秩
+    int rank2;           // 第二个模式的秩
+    int rank3;           // 第三个模式的秩
+
+    // 构造函数，提供默认值
+    TuckerConfig(
+        int _batch_size = 1,
+        int _in_features = 1,
+        int _out_features = 1,
+        int _rank1 = 1,
+        int _rank2 = 1,
+        int _rank3 = 1
+    ) : batch_size(_batch_size),
+        in_features(_in_features),
+        out_features(_out_features),
+        rank1(_rank1),
+        rank2(_rank2),
+        rank3(_rank3) {}
 };
 
 // Tucker ProcessingElement declaration
